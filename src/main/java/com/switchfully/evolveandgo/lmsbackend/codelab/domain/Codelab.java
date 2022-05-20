188d9a4b599c972e63108abfa808bc78fb120ed8
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-05-20 16:31:53 +0200

    COD 3: Creating entity (in progress)

--- src/main/java/com/switchfully/evolveandgo/lmsbackend/codelab/domain/Codelab.java
+++ src/main/java/com/switchfully/evolveandgo/lmsbackend/codelab/domain/Codelab.java
@@ -2,10 +2,26 @@ package com.switchfully.evolveandgo.lmsbackend.codelab.domain;
 
 import com.switchfully.evolveandgo.lmsbackend.codelab.domain.CodelabProgress;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CODELAB")
 public class Codelab {
 
-    private final String name;
-    private final CodelabProgress progress;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "codelab_seq")
+    @SequenceGenerator(name = "codelab_seq", sequenceName = "codelab_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    // Create second entity for Colab Progress
+    @OneToOne(mappedBy = "")
+    private CodelabProgress progress;
+
+    public Codelab() {
+    }
 
     public Codelab(String name, CodelabProgress progress) {
         this.name = name;
