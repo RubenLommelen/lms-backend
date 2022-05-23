caef79e41c8ea2f88747fbeaae2ebb6e4851cd96
Alperen2112 <a.alperen@outlook.com>
2022-05-23 15:16:43 +0200

    COD3- returning all codelabs

--- src/main/java/com/switchfully/evolveandgo/lmsbackend/codelab/service/CodelabService.java
+++ src/main/java/com/switchfully/evolveandgo/lmsbackend/codelab/service/CodelabService.java
@@ -1,8 +1,6 @@
 package com.switchfully.evolveandgo.lmsbackend.codelab.service;
 
-import com.switchfully.evolveandgo.lmsbackend.codelab.domain.Student;
-import com.switchfully.evolveandgo.lmsbackend.codelab.domain.StudentCodelabProgressJpaRepository;
-import com.switchfully.evolveandgo.lmsbackend.codelab.domain.StudentJpaRepository;
+import com.switchfully.evolveandgo.lmsbackend.codelab.domain.*;
 import com.switchfully.evolveandgo.lmsbackend.codelab.dto.StudentCodelabProgressDto;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +12,42 @@ public class CodelabService {
     private final StudentCodelabProgressMapper studentCodelabProgressMapper;
     private final StudentCodelabProgressJpaRepository studentCodelabProgressJpaRepository;
     private final StudentJpaRepository studentJpaRepository;
+    private final CodelabJpaRepository codelabJpaRepository;
 
-    public CodelabService(StudentCodelabProgressMapper studentCodelabProgressMapper, StudentCodelabProgressJpaRepository studentCodelabProgressJpaRepository, StudentJpaRepository studentJpaRepository) {
+    public CodelabService(StudentCodelabProgressMapper studentCodelabProgressMapper, StudentCodelabProgressJpaRepository studentCodelabProgressJpaRepository, StudentJpaRepository studentJpaRepository, CodelabJpaRepository codelabJpaRepository) {
         this.studentCodelabProgressMapper = studentCodelabProgressMapper;
         this.studentCodelabProgressJpaRepository = studentCodelabProgressJpaRepository;
         this.studentJpaRepository = studentJpaRepository;
+        this.codelabJpaRepository = codelabJpaRepository;
     }
 
     public List<StudentCodelabProgressDto> getCodelabsForStudent(Long id) {
         Student student = studentJpaRepository.findById(id).orElseThrow(() -> {
             throw new StudentNotFoundException(id);
         });
-        return studentCodelabProgressJpaRepository.findByStudent(student).stream()
-                .map(studentCodelab -> studentCodelabProgressMapper.toDto(studentCodelab))
+
+
+        List<StudentCodelabProgress> studentCodeLabProgressForStudent = studentCodelabProgressJpaRepository.findByStudent(student);
+        List<Codelab> codelabs = codelabJpaRepository
+                .findAll();
+
+        List<Long> codelabProgressIdList = studentCodeLabProgressForStudent.stream()
+                .map(studentCodelabProgress -> studentCodelabProgress.getCodelab().getId()).toList();
+        List<Codelab> filteredCodelabs = codelabs.stream()
+                .filter(codelab -> !codelabProgressIdList.contains(codelab.getId())).toList();
+
+        List<StudentCodelabProgress> codelabsNotStartedList = filteredCodelabs.stream()
+                .map(codelab -> new StudentCodelabProgress(CodelabProgress.NOT_STARTED, codelab, student))
                 .toList();
+        //List<StudentCodelabProgress> codelabsWithoutProgress = codelabsNotStartedList.stream()
+        //        .filter(codelab -> !studentCodeLabProgressForStudent.contains(codelab))
+        //        .toList();
+//
+        studentCodeLabProgressForStudent.addAll(codelabsNotStartedList);
+
+        return studentCodelabProgressMapper.toDtoList(studentCodeLabProgressForStudent);
+
+
     }
 }
+
