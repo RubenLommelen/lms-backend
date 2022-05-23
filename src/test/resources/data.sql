c680eedd8607c7c1a2046312423765293aab9dc1
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-05-23 12:04:16 +0200

    COD 3: Added student implementation

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,11 +1,14 @@
+INSERT INTO STUDENT
+values (nextVal('student_seq'), 'rinaldo@spaghetto.be', 'Rinaldo', 'Spaghetto');
+
 INSERT INTO codelab
 values (nextVal('codelab_seq'), 'Rinaldo');
 
 INSERT INTO codelab_progress
-values (nextVal('codelab_progress_seq'), currval('codelab_seq'), 'DONE');
+values (nextVal('codelab_progress_seq'), currval('codelab_seq'), 'DONE', currval('student_seq'));
 
 INSERT INTO codelab
 values (nextVal('codelab_seq'), 'Ronaldo');
 
 INSERT INTO codelab_progress
-values (nextVal('codelab_progress_seq'), currval('codelab_seq'), 'BUSY');
+values (nextVal('codelab_progress_seq'), currval('codelab_seq'), 'BUSY', currval('student_seq'));
