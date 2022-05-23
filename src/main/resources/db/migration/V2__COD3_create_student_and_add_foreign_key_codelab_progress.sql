598f37914daa1203ce0091a9b630bd44c6d4d518
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-05-23 12:03:48 +0200

    COD 3: Added V2 flyway for creating student table and adding foreing key to codelab_progress to student

---
+++ src/main/resources/db/migration/V2__COD3_create_student_and_add_foreign_key_codelab_progress.sql
@@ -0,0 +1,11 @@
+CREATE TABLE STUDENT (
+    ID integer not null primary key,
+    EMAIL varchar(256) not null,
+    DISPLAY_NAME varchar(256) not null,
+    PASSWORD varchar(256) not null
+);
+create sequence student_seq start with 1 increment by 1;
+
+ALTER TABLE codelab_progress ADD COLUMN FK_STUDENT INTEGER;
+ALTER TABLE codelab_progress ADD CONSTRAINT FK_STUDENT_TO_CODELAB_PROGRESS
+    FOREIGN KEY (FK_STUDENT) REFERENCES codelab_progress (ID);
