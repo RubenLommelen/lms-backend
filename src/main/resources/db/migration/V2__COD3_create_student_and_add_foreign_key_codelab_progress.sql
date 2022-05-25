1ba0d400812b003a8c3a2b8515f54e4a08d66d17
Rinaldo <menichettir@hotmail.com>
2022-05-25 11:23:20 +0200

    corrected mistake in foreign key for student

--- src/main/resources/db/migration/V2__COD3_create_student_and_add_foreign_key_codelab_progress.sql
+++ src/main/resources/db/migration/V2__COD3_create_student_and_add_foreign_key_codelab_progress.sql
@@ -8,4 +8,4 @@ create sequence student_seq start with 1 increment by 1;
 
 ALTER TABLE codelab_progress ADD COLUMN FK_STUDENT INTEGER;
 ALTER TABLE codelab_progress ADD CONSTRAINT FK_STUDENT_TO_CODELAB_PROGRESS
-    FOREIGN KEY (FK_STUDENT) REFERENCES codelab_progress (ID);
+    FOREIGN KEY (FK_STUDENT) REFERENCES student (ID);
