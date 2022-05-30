a9f7ddeaa13bcb2684beb47d5a3c5e77ec80c6fd
Baker El Muhur <b.elmuhur@gmail.com>
2022-05-30 16:44:17 +0200

    PRO-5-A: trying to get grouped data from database using JPQL

--- src/main/java/com/switchfully/evolveandgo/lmsbackend/codelab/domain/StudentCodelabProgressJpaRepository.java
+++ src/main/java/com/switchfully/evolveandgo/lmsbackend/codelab/domain/StudentCodelabProgressJpaRepository.java
@@ -2,6 +2,7 @@ package com.switchfully.evolveandgo.lmsbackend.codelab.domain;
 
 import com.switchfully.evolveandgo.lmsbackend.progress.domain.ProgressOverview;
 import com.switchfully.evolveandgo.lmsbackend.student.domain.Student;
+import com.switchfully.evolveandgo.lmsbackend.codelab.domain.CodelabProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,10 +11,17 @@ import java.util.List;
 public interface StudentCodelabProgressJpaRepository extends JpaRepository<StudentCodelabProgress, Long> {
     List<StudentCodelabProgress> findByStudent(Student student);
 
-    @Query(value = "SELECT fk_student as studentId, count(progress) as numberOfCompletedCodelabs " +
-            "FROM codelab_progress " +
-            "where progress in ('DONE', 'FEEDBACK_NEEDED') " +
-            "group by studentId;", nativeQuery = true)
+//    @Query(value = "SELECT fk_student as studentId, count(progress) as numberOfCompletedCodelabs " +
+//            "FROM codelab_progress " +
+//            "where progress in ('DONE', 'FEEDBACK_NEEDED') " +
+//            "group by studentId;", nativeQuery = true)
+    @Query(value = "SELECT new com.switchfully.evolveandgo.lmsbackend.progress.domain.ProgressOverview(c.student.id, count(c)) as numberOfCompletedCodelabs " +
+            "FROM StudentCodelabProgress c " +
+            "where c.progress in (com.switchfully.evolveandgo.lmsbackend.codelab.domain.CodelabProgress.DONE, com.switchfully.evolveandgo.lmsbackend.codelab.domain.CodelabProgress.FEEDBACK_NEEDED) " +
+            "group by c.student.id")
     List<ProgressOverview> findProgressOverview();
+
+    // @Query("select c from Country c where c.name = :name")
+    //Country findByName(@Param("name") String name);
 
 }
--- src/main/java/com/switchfully/evolveandgo/lmsbackend/progress/domain/ProgressOverview.java
+++ src/main/java/com/switchfully/evolveandgo/lmsbackend/progress/domain/ProgressOverview.java
@@ -1,31 +1,32 @@
 package com.switchfully.evolveandgo.lmsbackend.progress.domain;
 
-public interface ProgressOverview {
 
-    Long getStudentId();
-    Integer getNumberOfCompletedCodelabs();
+public class ProgressOverview {
+//
+//    Long getStudentId();
+//    Integer getNumberOfCompletedCodelabs();
 
-//    private final Long studentId;
-//    private final int numberOfCompletedCodelabs;
+    private final Long studentId;
+    private final Long numberOfCompletedCodelabs;
 
-//    public ProgressOverview(Long studentId, int numberOfCompletedCodelabs) {
-//        this.studentId = studentId;
-//        this.numberOfCompletedCodelabs = numberOfCompletedCodelabs;
-//    }
-//
-//    public Long getStudentId() {
-//        return studentId;
-//    }
-//
-//    public int getNumberOfCompletedCodelabs() {
-//        return numberOfCompletedCodelabs;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "ProgressOverview{" +
-//                "studentId=" + studentId +
-//                ", numberOfCompletedCodelabs=" + numberOfCompletedCodelabs +
-//                '}';
-//    }
+    public ProgressOverview(Long studentId, Long numberOfCompletedCodelabs) {
+        this.studentId = studentId;
+        this.numberOfCompletedCodelabs = numberOfCompletedCodelabs;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public Long getNumberOfCompletedCodelabs() {
+        return numberOfCompletedCodelabs;
+    }
+
+    @Override
+    public String toString() {
+        return "ProgressOverview{" +
+                "studentId=" + studentId +
+                ", numberOfCompletedCodelabs=" + numberOfCompletedCodelabs +
+                '}';
+    }
 }
